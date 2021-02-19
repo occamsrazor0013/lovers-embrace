a21f0550cc9b09db5a4248e2ab20a4afc3b37edb
David Chan <dcdaveychan11@gmail.com>
2021-02-19 04:14:49 -0500

    checkout, forms, payment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/rose.png'
@@ -6,16 +6,35 @@ import useStyles from './styles.js';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = ({ totalItems }) => {
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
     const classes = useStyles();
     const location = useLocation();
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    
+    const renderMobileMenu = (
+        <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
+            <MenuItem>
+                <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                    <Badge badgeContent={totalItems} color="secondary">
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
+                <p>Cart</p>
+            </MenuItem>
+        </Menu>
+    );
 
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="Lover's Embrace" height= "25px" className={classes.image} />
-                            Lover's Embrace
+                        <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Lover's Embrace
                     </Typography>
                     <div className={classes.grow} />
                     {location.pathname === '/' && (
@@ -25,11 +44,13 @@ const Navbar = ({ totalItems }) => {
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div> )}
+                    </div>
+                    )}
                 </Toolbar>
             </AppBar>
+            {renderMobileMenu}
         </>
-    )
-}
+    );
+};
 
 export default Navbar
